34b18ac75fb691fdc0c1272cf809901132df7d0c
Miss-Nikita <bisennikita770@gmail.com>
2025-02-04 19:31:35 +0530

    ...

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Sidenav from "./templates/Sidenav";
 import Topnav from "./templates/Topnav";
 import axios from "../utils/axios";
+import Header from "./templates/Header";
 
 const Home = () => {
   document.title = "The_film_Vault || Homepage";
@@ -13,27 +14,28 @@ const Home = () => {
       const { data } = await axios.get(`/trending/all/day`);
       console.log(data);
 
-     let randomdata =  data.results[Math.random() * data.results.length];
+     let randomdata =  data.results[(Math.random() * data.results.length).toFixed()]
       setwallpaper(randomdata);
     } catch (error) {
       console.error("Error fetching search results:", error.message);
     }
   };
-  console.log(wallpaper);
+  // console.log(wallpaper);
 
   useEffect(() => {
     !wallpaper && GetHeaderWallpaper();
   }, []);
 
-  return (
+  return wallpaper ? (
     <>
       <Sidenav />
 
       <div className="w-[80%] h-full ">
         <Topnav />
+        <Header data={wallpaper} />
       </div>
     </>
-  );
+  ):( <h1>Loading...</h1>)
 };
 
 export default Home;
--- src/components/templates/Header.jsx
+++ src/components/templates/Header.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Header = () => {
+const Header = ({ data }) => {
+  console.log(data);
+
   return (
-    <div className='w-full h-[50vh] '>
+    <div
+      style={{
+        background: `linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.7),rgba(0,0,0,.9)), url(https://image.tmdb.org/t/p/original/${
+          data.backdrop_path || data.profile_path
+        }) `,
+      }}
+      className="w-full h-[50vh] "
+    >
       Header
-      
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
